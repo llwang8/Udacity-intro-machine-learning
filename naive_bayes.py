dd80b9a869cc88bd86fe5fd220efeb4ac96b558b
llwang8 <wangxbl56@gmail.com>
2016-12-10 20:10:52 -0500

    finish Naive Bayes and SVM lesson

---
+++ naive_bayes.py
@@ -0,0 +1,125 @@
+# Intro to Machine Learning
+# Lesson.1: Naive Bayes
+
+#===========================================
+
+# Quiz: GaussianNB Deployment
+
+
+# StudentMain.py
+
+#!/usr/bin/python
+
+""" Complete the code in ClassifyNB.py with the sklearn
+    Naive Bayes classifier to classify the terrain data.
+
+    The objective of this exercise is to recreate the decision
+    boundary found in the lesson video, and make a plot that
+    visually shows the decision boundary """
+
+
+from prep_terrain_data import makeTerrainData
+from class_vis import prettyPicture, output_image
+from ClassifyNB import classify
+
+import numpy as np
+import pylab as pl
+
+
+features_train, labels_train, features_test, labels_test = makeTerrainData()
+
+### the training data (features_train, labels_train) have both "fast" and "slow" points mixed
+### in together--separate them so we can give them different colors in the scatterplot,
+### and visually identify them
+grade_fast = [features_train[ii][0] for ii in range(0, len(features_train)) if labels_train[ii]==0]
+bumpy_fast = [features_train[ii][1] for ii in range(0, len(features_train)) if labels_train[ii]==0]
+grade_slow = [features_train[ii][0] for ii in range(0, len(features_train)) if labels_train[ii]==1]
+bumpy_slow = [features_train[ii][1] for ii in range(0, len(features_train)) if labels_train[ii]==1]
+
+
+# You will need to complete this function imported from the ClassifyNB script.
+# Be sure to change to that code tab to complete this quiz.
+clf = classify(features_train, labels_train)
+clf.predict(features_test)
+
+
+### draw the decision boundary with the text points overlaid
+prettyPicture(clf, features_test, labels_test)
+output_image("test.png", "png", open("test.png", "rb").read())
+
+
+#----------------------------
+# classifyNB.py
+
+def classify(features_train, labels_train):
+    ### import the sklearn module for GaussianNB
+    ### create classifier
+    ### fit the classifier on the training features and labels
+    ### return the fit classifier
+
+
+    ### your code goes here!
+
+    from sklearn.naive_bayes import GaussianNB
+    classifier =  GaussianNB()
+    classifier.fit(features_train, labels_train)
+    ###GaussianNB(priors=None)
+    return classifier
+
+#======================================
+
+# Quiz: Calculating NB Accuracy
+
+# classify.py
+def NBAccuracy(features_train, labels_train, features_test, labels_test):
+    """ compute the accuracy of your Naive Bayes classifier """
+    ### import the sklearn module for GaussianNB
+    from sklearn.naive_bayes import GaussianNB
+    from sklearn.metrics import accuracy_score
+    import numpy as np
+
+    ### create classifier
+    clf = GaussianNB()
+
+    ### fit the classifier on the training features and labels
+    clf.fit(features_train, labels_train)
+
+    ### use the trained classifier to predict labels for the test features
+    pred = clf.predict(features_test)
+    #print pred
+
+    ### calculate and return the accuracy on the test data
+    ### this is slightly different than the example,
+    ### where we just print the accuracy
+    ### you might need to import an sklearn module
+
+    ### first method
+    accuracy = accuracy_score(labels_test, pred)
+
+    ### second method
+    #pred = np.array(pred)
+    #labels_train = np.array(labels_train)
+    #accuracy = np.sum(pred == labels_train) / count(labels_train)
+    return accuracy
+
+
+# studentCode.py
+
+from class_vis import prettyPicture
+from prep_terrain_data import makeTerrainData
+from classify import NBAccuracy
+
+import matplotlib.pyplot as plt
+import numpy as np
+import pylab as pl
+
+
+features_train, labels_train, features_test, labels_test = makeTerrainData()
+
+def submitAccuracy():
+    accuracy = NBAccuracy(features_train, labels_train, features_test, labels_test)
+    return accuracy
+
+
+
+
---
+++ ud120-projects-master/svm/svm_author_id.py
@@ -0,0 +1,100 @@
+#!/usr/bin/python
+
+"""
+    This is the code to accompany the Lesson 2 (SVM) mini-project.
+
+    Use a SVM to identify emails from the Enron corpus by their authors:
+    Sara has label 0
+    Chris has label 1
+"""
+
+import sys
+from time import time
+sys.path.append("../tools/")
+from email_preprocess import preprocess
+
+
+### features_train and features_test are the features for the training
+### and testing datasets, respectively
+### labels_train and labels_test are the corresponding item labels
+features_train, features_test, labels_train, labels_test = preprocess()
+
+
+#########################################################
+### your code goes here ###
+from sklearn.svm import SVC
+from sklearn.metrics import accuracy_score
+
+clf = SVC(kernel='rbf', C=10000)
+#features_train = features_train[:len(features_train)/100]
+#labels_train = labels_train[:len(labels_train)/100]
+
+t0 = time()
+clf.fit(features_train, labels_train)
+print "training time:", round(time()-t0, 3), "s"
+
+t0 = time()
+pred = clf.predict(features_test)
+print "predict time:", round(time()-t0, 3), "s"
+accuracy = accuracy_score(pred, labels_test)
+
+print accuracy
+
+#print "pred 10th:", pred[10]
+#print "pred 26th:", pred[26]
+#print "pred 50th:", pred[50]
+
+print "num for Chris:", sum(pred)
+print "num for Sara:", len(pred)-sum(pred)
+
+
+#-------------------------------------------
+# full trainin gdataset with linear kernel
+# 0.984072810011
+
+# 1% training dataset with linear kernel
+# training time: 0.095 s
+# predict time: 1.002 s
+# 0.884527872582
+
+# 1% training dataset with rbf kernel
+# training time: 0.113 s
+# predict time: 1.132 s
+# 0.616040955631
+
+# 1% training dataset with rbf kernel (C=10)
+# training time: 0.112 s
+# predict time: 1.145 s
+# 0.616040955631
+
+# 1% training dataset with rbf kernel (C=100)
+#training time: 0.115 s
+#predict time: 1.132 s
+#0.616040955631
+
+# 1% training dataset with rbf kernel (C=1000)
+# training time: 0.11 s
+# predict time: 1.08 s
+# 0.821387940842
+
+# 1% training dataset with rbf kernel (C=10000)
+# training time: 0.109 s
+# predict time: 0.924 s
+# 0.892491467577
+
+# full trainin gdataset with rbf kernel (C=10000)
+# training time: 108.159 s
+# predict time: 10.772 s
+# 0.990898748578
+
+# 1% training dataset with rbf kernel (C=10000)
+# pred 10th: 1
+# pred 26th: 0
+# pred 50th: 1
+# training time: 0.113 s
+# predict time: 0.979 s
+# 0.892491467577
+
+#########################################################
+
+
